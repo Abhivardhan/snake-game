73d9e5e72c5d3c4a664340bf520a85bf0f65c39f
Abhivardhan <saddikuti.abhivardhan@gmail.com>
2020-05-05 18:10:27 +0530

    keys support

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 function init() {
     canvas = document.getElementById("snakeBoard");
     canvas.width = 1000;
@@ -100,57 +101,25 @@ function init() {
         }
     }
 
-    function getTouches(evt) {
-        return evt.touches || evt.originalEvent.touches;
-      }                                                     
-      
-    function handleTouchStart(evt) {
-        const firstTouch = getTouches(evt)[0];                                      
-        xDown = firstTouch.clientX;                                      
-        yDown = firstTouch.clientY;                                      
-    };    
-
-    function handleTouchMove(evt) {
-        if (!xDown || !yDown ) {
-            return;
-        }
-    
-        var xUp = evt.touches[0].clientX;                                    
-        var yUp = evt.touches[0].clientY;
-    
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
-    
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-            if ( xDiff > 0 ) {
-               keyPressed({
-                   key: "left"
-               });
-            } else {
-                keyPressed({
-                    key: "right"
-                });
-            }                       
-        } else {
-            if ( yDiff > 0 ) {
-                keyPressed({
-                    key: "up"
-                });
-            } else { 
-                keyPressed({
-                    key: "down"
-                });
-            }                                                                 
-        }
-
-        xDown = null;
-        yDown = null;                                             
-    };
-
     document.addEventListener('keydown', keyPressed);
-    document.addEventListener('touchstart', handleTouchStart, false);        
-    document.addEventListener('touchmove', handleTouchMove, false);
 }
+
+function left() {
+    snake.direction = "left";
+}
+
+function up() {
+    snake.direction = "up";
+}
+
+function right() {
+    snake.direction = "right";
+}
+
+function down() {
+    snake.direction = "down";
+}
+
 
 function draw() {
     pen.clearRect(0, 0, canvas.width, canvas.height);
@@ -181,7 +150,7 @@ function getRandomFood() {
 function gameloop() {
 
 	if(game_over==true) {
-		clearInterval(f);
+		clearInterval(game);
 		alert("Game Over");
 		return;
 	}
@@ -191,5 +160,5 @@ function gameloop() {
 
 init();
 
-let f = setInterval(gameloop, 100);
+let game = setInterval(gameloop, 100);
 
